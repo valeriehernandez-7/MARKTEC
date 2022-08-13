821979afb81962e171a0aacc62a1979d9bdd3fb2
valeriehernandez-7 <valerie.herfer@gmail.com>
2022-08-13 12:52:37 -0600

    DataTest Script

--- src/database/sql-queries/scripts/DataTest.sql
+++ src/database/sql-queries/scripts/DataTest.sql
@@ -1,22 +1,46 @@
 USE [MARKTEC]
 GO
 
-/* Display data from User table */
-SELECT * FROM [dbo].[User];
-/* Display data from ItemType table */
-SELECT * FROM [dbo].[ItemType];
+/* Display data from ErrorLog table */
+SELECT * FROM [dbo].[ErrorLog];
+GO
+/* Display data from EventLog table */
+SELECT * FROM [dbo].[EventLog];
+GO
+/* Display data from EventType table */
+SELECT * FROM [dbo].[EventType];
+GO
 /* Display data from Item table */
 SELECT * FROM [dbo].[Item];
-
-/*Deletes data from Usuario table and restart the Id counter */
-DELETE FROM [dbo].[User];
-DBCC CHECKIDENT ('[dbo].[User]', RESEED, 0);
 GO
-/*Deletes data from ItemType table and restart the Id counter */
-DELETE FROM [dbo].[ItemType];
-DBCC CHECKIDENT ('[dbo].[ItemType]', RESEED, 0);
+/* Display data from ItemType table */
+SELECT * FROM [dbo].[ItemType];
+GO
+/* Display data from User table */
+SELECT * FROM [dbo].[User];
+GO
+
+/*Deletes data from ErrorLog table and restart the Id counter */
+DELETE FROM [dbo].[ErrorLog];
+DBCC CHECKIDENT ('[dbo].[ErrorLog]', RESEED, 0);
+GO
+/*Deletes data from EventLog table and restart the Id counter */
+DELETE FROM [dbo].[EventLog];
+DBCC CHECKIDENT ('[dbo].[EventLog]', RESEED, 0);
+GO
+/*Deletes data from EventType table and restart the Id counter */
+DELETE FROM [dbo].[EventType];
+DBCC CHECKIDENT ('[dbo].[EventType]', RESEED, 0);
 GO
 /*Deletes data from Item table and restart the Id counter */
 DELETE FROM [dbo].[Item];
 DBCC CHECKIDENT ('[dbo].[Item]', RESEED, 0);
+GO
+/*Deletes data from ItemCategory table and restart the Id counter */
+DELETE FROM [dbo].[ItemCategory];
+DBCC CHECKIDENT ('[dbo].[ItemCategory]', RESEED, 0);
+GO
+/*Deletes data from User table and restart the Id counter */
+DELETE FROM [dbo].[User];
+DBCC CHECKIDENT ('[dbo].[User]', RESEED, 0);
 GO
