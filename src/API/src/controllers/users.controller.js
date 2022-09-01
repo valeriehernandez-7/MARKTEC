ab48ea344ca13a13f8a1dadefa0e4d04cb0caf35
Erick <efmz2000@outlook.es>
2022-08-31 22:02:05 -0600

    login

--- src/API/src/controllers/users.controller.js
+++ src/API/src/controllers/users.controller.js
@@ -5,10 +5,10 @@ export const login = async (req, res) => {
     const pool= await getConection()
     const {username, password} = req.body;   
     const result= await pool.request()
-                    .input('@inUsername', sql.NVARCHAR(64),username).                    
-                    output('outResultCode', sql.Int).
+                    .input('inUsername', sql.NVARCHAR(64),username)
+                    .input('inPassword', sql.NVARCHAR(64),password)                    
+                    .output('outResultCode', sql.Int).
                     execute('SP_Login');
-    console.log('itemCategoryName');
-    res.json(result.recordset);
+    res.json(result.output.outResultCode);
         
 }
