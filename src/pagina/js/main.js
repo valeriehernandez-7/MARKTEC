93d0178f17b0cf13efa363708308f46c000e54fe
Erick <efmz2000@outlook.es>
2022-09-02 16:37:34 -0600

    funciones de giltrado

---
+++ src/pagina/js/main.js
@@ -0,0 +1,106 @@
+var cant_elementos;
+//;ista inicial
+$(document).ready(function(){
+    var url = "http://localhost:8000/itemsCategory"
+    const options = {
+    method: "get",
+    headers: {"Content-Type": "application/json"},
+    };
+    fetch(url, options).then(response => response.json())
+    .then(response => {
+      console.log(response);
+      $("#tableBody > tbody").empty();
+      cant_elementos=response.length
+      for (var i = 0; i < response.length; i++) {
+        $("#tablaItems ").append("<tr><td> "+response[i].ID +"</td><td>"+response[i].Description +" </td><td> "+response[i].ID +" </td><td> "+response[i].Price +" </td></tr>");
+      }
+
+  }).catch(e => {
+      console.log(e);
+  });
+})
+//boton filto categoria
+function catfil() {
+  var category = $("#cat").val();
+  var url = "http://localhost:8000/itemsCategory?"
+  if (category != ""){
+      url= url+new URLSearchParams({
+        itemCategoryName:category,
+      })
+  }
+  if(category.includes(";") ){
+    window.alert("No debe incluir ;");
+    return;
+  }
+  const options = {
+  method: "get",
+  headers: {"Content-Type": "application/json"},
+  };
+  // Petición HTTP
+  fetch(url, options).then(response => response.json())
+  .then(response => {
+    console.log(response);
+    $("#tablaItems > tbody").empty();
+    $("#tablaItems ").append("<tr><th>ID</th><th>Descripcion</th><th> Categoria</th><th>Precio</th></tr>");
+    for (var i = 0; i < response.length; i++) {
+      $("#tablaItems ").append("<tr><td>"+response[i].ID +"</td><td>"+response[i].Description +"</td><td>"+response[i].ID +"</td><td>"+response[i].Price +"</td></tr>");
+    }
+}).catch(e => {
+    console.log(e);
+});
+}
+function descfil() {
+  var itemDescription = $("#desc").val();
+  var url = "http://localhost:8000/itemsDescription?"
+  if (itemDescription != ""){
+      url= url+new URLSearchParams({
+        itemDescription:itemDescription,
+      })
+  }
+  if(itemDescription.includes(";") ){
+    window.alert("No debe incluir ;");
+    return;
+  }
+  const options = {
+  method: "get",
+  headers: {"Content-Type": "application/json"},
+  };
+  // Petición HTTP
+  fetch(url, options).then(response => response.json())
+  .then(response => {
+    console.log(response);
+    $("#tablaItems ").empty();
+    $("#tablaItems").append("<tr><th>ID</th><th>Descripcion</th><th> Categoria</th><th>Precio</th></tr>");
+    for (var i = 0; i < response.length; i++) {
+      $("#tablaItems").append("<tr><td>"+response[i].ID +"</td><td>"+response[i].Description +"</td><td>"+response[i].ID +"</td><td>"+response[i].Price +"</td></tr>");
+    }
+}).catch(e => {
+    console.log(e);
+});
+}
+
+function cant() {
+  var itemAmount = $("#cant").val();
+  var url = "http://localhost:8000/itemsAmount?"
+  if (itemAmount != ""){
+      url= url+new URLSearchParams({
+        itemAmount:itemAmount,
+      })
+  }
+  const options = {
+  method: "get",
+  headers: {"Content-Type": "application/json"},
+  };
+  // Petición HTTP
+  fetch(url, options).then(response => response.json())
+  .then(response => {
+    console.log(response);
+    $("#tablaItems ").empty();
+    $("#tablaItems").append("<tr><th>ID</th><th>Descripcion</th><th> Categoria</th><th>Precio</th></tr>");
+    for (var i = 0; i < response.length; i++) {
+      $("#tablaItems").append("<tr><td>"+response[i].ID +"</td><td>"+response[i].Description +"</td><td>"+response[i].ID +"</td><td>"+response[i].Price +"</td></tr>");
+    }
+}).catch(e => {
+    console.log(e);
+});
+}
